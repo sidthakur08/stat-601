f9c0022b87f2968ec7b264292f7d079e95d90bf8
sidthakur08 <41535376+sidthakur08@users.noreply.github.com>
2021-03-04 18:54:26 -0800

    week6

---
+++ week6_application.R
@@ -0,0 +1,23 @@
+#[, 1]	mpg	Miles/(US) gallon
+#[, 2]	cyl	Number of cylinders
+#[, 3]	disp	Displacement (cu.in.)
+#[, 4]	hp	Gross horsepower
+#[, 5]	drat	Rear axle ratio
+#[, 6]	wt	Weight (1000 lbs)
+#[, 7]	qsec	1/4 mile time
+#[, 8]	vs	Engine (0 = V-shaped, 1 = straight)
+#[, 9]	am	Transmission (0 = automatic, 1 = manual)
+#[,10]	gear	Number of forward gears
+#[,11]	carb	Number of carburetors
+
+mtcars
+library(ggplot2)
+
+colnames(mtcars)
+
+head(mtcars)
+
+# cars with cylinders
+ggplot(mtcars,aes(cyl)) + 
+  geom_histogram(fill="cornflowerblue",color="white",bins=3)
+
